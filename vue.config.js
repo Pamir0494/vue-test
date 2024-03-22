0b28f7d2d3cae4be0b040b5d31d61359ba5b2f6c
Pamir0494 <bulokomoko@gmail.com>
2024-03-22 19:27:45 +0400

    v 1.0

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath:'/TestVue/',
+  publicPath:`/vue-test/`,
   transpileDependencies: true,
   lintOnSave:false
 })
